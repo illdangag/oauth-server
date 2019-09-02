0b84d66811bbbf8cf26ff354e8062137595c87f7
ybchoi <ybchoi@forcs.com>
2019-09-02 18:04:27 +0900

    tslint 수정

--- frontend/store.ts
+++ frontend/store.ts
@@ -6,7 +6,7 @@ export const exampleInitialState = {
   lastUpdate: 0,
   light: false,
   count: 0,
-};
+}
 
 export type State = {
   lastUpdate: number,
@@ -19,7 +19,7 @@ export const actionTypes = {
   INCREMENT: 'INCREMENT',
   DECREMENT: 'DECREMENT',
   RESET: 'RESET',
-};
+}
 
 // REDUCERS
 export const reducer = (state = exampleInitialState, action: any) => {
@@ -28,34 +28,34 @@ export const reducer = (state = exampleInitialState, action: any) => {
       return Object.assign({}, state, {
         lastUpdate: action.ts,
         light: !!action.light,
-      });
+      })
     case actionTypes.INCREMENT:
       return Object.assign({}, state, {
         count: state.count + 1,
-      });
+      })
     case actionTypes.DECREMENT:
       return Object.assign({}, state, {
         count: state.count - 1,
-      });
+      })
     case actionTypes.RESET:
       return Object.assign({}, state, {
         count: exampleInitialState.count,
-      });
+      })
     default: return state
   }
-};
+}
 
 // ACTIONS
 export const serverRenderClock = (isServer: boolean) => (dispatch: Dispatch) => {
   return dispatch({ type: actionTypes.TICK, light: !isServer, ts: Date.now(), })
-};
+}
 
 export const startClock = (dispatch: Dispatch) => {
   return setInterval(() => {
     // Dispatch `TICK` every 1 second
     dispatch({ type: actionTypes.TICK, light: true, ts: Date.now(), })
   }, 1000)
-};
+}
 
 export function initializeStore(initialState = exampleInitialState) {
   return createStore(reducer, initialState, composeWithDevTools(applyMiddleware(thunkMiddleware)))
